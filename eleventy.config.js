0ac2b3b3a9229e8587e9a5e1e08dfd6d7f0626c3
Ajay Gupta <ajay.gupta@ajayg.co>
2024-12-13 21:49:23 +0530

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,14 @@
+export default function (config) {
+  config.addPassthroughCopy("source/assets");
+  config.addPassthroughCopy("source/robots.txt");
+  config.addPassthroughCopy("source/sitemap.xml");
+
+  return {
+    dir: {
+      output: "dist",
+      input: "source",
+      includes: "includes",
+      data: "data",
+    },
+  };
+}
